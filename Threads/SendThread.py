b997f474b56dd3782a592b3807552d2133caf264
Artem Turaev <20820143+Tesseract98@users.noreply.github.com>
2023-02-19 13:41:43 +0400

    another bot concept

--- Threads/SendThread.py
+++ Threads/SendThread.py
@@ -1,37 +1,33 @@
-from threading import Thread
-import time
 import datetime
-from Tools.NecessaryMethods import change_db, logger_error, write_msg
-from Tools.ConstantsValue import digits, len_digits_lst
+import logging as log
+import random
+import time
+from threading import Thread
+
+from Tools.NecessaryMethods import change_db, dict_to_str, calculate
 from config.constants import ID_ART
 
 
 class ThreadSendMsg(Thread):
-    def __init__(self, parameters, day_global):
+    def __init__(self, vk, parameters):
         Thread.__init__(self)
-        self.day_global = day_global
+        self.__vk = vk
         self.parameters = parameters
 
     def run(self):
-        day = self.day_global
-        parameters = self.parameters
+        change_per_month = self.parameters['change_per_month']
         while True:
             var = datetime.datetime.now()
-            if var.strftime('%d') != day:
-                day = var.strftime('%d')
-                parameters['date'] = day
-                change_db(parameters)
-            if '11:17' <= var.strftime('%H:%M') <= '11:18':
-                write_msg(ID_ART, '{0}'.format('STOP!'))
-                # write_msg(ID_SERG, '{0} \general_amount {1}'.format(digits[parameters['index']], 'STOP!'))
-                time.sleep(360)
-            if '16:00' <= var.strftime('%H:%M') <= '16:01':
-                write_msg(ID_ART, '{0} \n {1}'.format(digits[parameters['index']], 'Думай головой!!!'))
-                # write_msg(ID_SERG, '{0} \general_amount {1}'.format(digits[parameters['index']], 'Думай головой!!!'))
-                if parameters['index'] + 1 < len_digits_lst:
-                    # parameters['index'] += 1
-                    change_db(parameters)
-                else:
-                    logger_error('Not enough unique values, list overload')
-                time.sleep(790)
-            time.sleep(41)
+            if (var + datetime.timedelta(days=1)).strftime('%d') < var.strftime('%d'):
+                calculate(self.parameters['variable'], change_per_month)
+                change_per_month['СКМ'] = self.parameters['variable']['СС']
+                self.write_msg(ID_ART, dict_to_str(change_per_month))
+                change_db(self.parameters)
+            if (var - datetime.timedelta(days=1)).strftime('%d') > var.strftime('%d'):
+                change_per_month['СНМ'] = self.parameters['variable']['СС']
+                change_db(self.parameters)
+            time.sleep(86400)
+
+    def write_msg(self, user_id, message):
+        log.info('{}: user: id{}'.format(datetime.datetime.now().strftime("%d-%m-%Y %H:%M"), user_id))
+        self.__vk.method('messages.send', {'user_id': user_id, 'message': message, "random_id": random.randint(10, 1 << 32)})
